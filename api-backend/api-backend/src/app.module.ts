a62cf390b8e72102afc6c11bb71535ab644d2518
Isabela959 <isabelaramosdasilva15@gmail.com>
2025-09-12 16:54:05 -0300

    12/09/2025

--- api-backend/api-backend/src/app.module.ts
+++ api-backend/api-backend/src/app.module.ts
@@ -5,8 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
-import { UsersService } from './users/users.service';
-import { AuthModule } from './auth/auth.module';
+
 
 @Module({
   imports: [
@@ -18,11 +17,10 @@ import { AuthModule } from './auth/auth.module';
       password: '1234', // <-- MUITO IMPORTANTE: Coloque a senha que você configurou na instalação do MySQL
       database: 'products3', // O nome do banco que criamos no Workbench
       entities: [__dirname + '/**/*.entity{.ts,.js}'], // Encontra as entidades automaticamente
-      synchronize: false, // Apenas para desenvolvimento! Cria as tabelas automaticamente.
+      synchronize: true, // Apenas para desenvolvimento! Cria as tabelas automaticamente.
     }),
     ProductsModule,
     UsersModule,
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
